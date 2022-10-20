ceca6ad7b2941e8cfbace34f11e5cf8f422a0e5d
Anderson Sales <andersonsales.mg@gmail.com>
2022-10-20 15:23:08 -0300

    project finished

--- StormViewApp/StormViewApp/DetailViewController.swift
+++ StormViewApp/StormViewApp/DetailViewController.swift
@@ -16,8 +16,19 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        title = image
+        navigationItem.largeTitleDisplayMode = .never
+        
         if let imageToLoad = image {
             imageView.image = UIImage(named: imageToLoad)
         }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        navigationController?.hidesBarsOnTap = false
     }
 }
